3ebafa33b62473eca0571cb4b8d05defa8ec6d55
Melanie Rodriguez <melarodrigg@gmail.com>
2025-12-14 15:17:32 -0600

    Implemented useRef, useId and useContext

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,5 +1,17 @@
+import { createContext, useState } from 'react';
+
 // TODO: Create a ThemeContext using createContext()
 // TODO: Define and export a ThemeProvider that will manage theme state
 
 // Placeholder export for now
-export const ThemeContext = {};
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+    {children}
+    </ThemeContext.Provider>
+  );
+}
